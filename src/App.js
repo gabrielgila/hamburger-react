c2e0bab5ee79091a99754e4cd863effb8d6aab86
mgila <marianogila@gmail.com>
2020-09-29 10:18:08 -0700

    children

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Person from './Person/Person';
 function App() {
   return (
     <div className="App">
+        <h1>Is working</h1>
+        <button>Switch Name</button>
         <Person name="Max" age="29">My Hobies: Racing</Person>
+        <Person name="Stephanie" age="26" />
     </div>
   );
 }
