bcacfed7731ff155958ffe56b74d6902dc6c0dd3
mgila <marianogila@gmail.com>
2020-09-29 07:41:05 -0700

    starts project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <h1>BLA BLA BLA BLA</h1>
+      <Person />
     </div>
   );
 }
