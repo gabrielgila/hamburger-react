71505788aa4391e371e83880edfc16e022bd26d4
mgila <marianogila@gmail.com>
2020-10-15 07:12:14 -0700

    aux

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
+
+import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.module.css';
+
 
 class Person extends Component {
     render() {
-        return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age}</p>
-                <p>{this.props.children}</p>
+        return(
+            <Aux>
+                <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age}</p>,
+                <p>{this.props.children}</p>,
                 <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </div>
-        );
+            </Aux>
+        )
     }
 };
 
